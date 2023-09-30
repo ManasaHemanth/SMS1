33e08ebd42c495567aa09c3703f33c87fa32de22
ManasaHemanth <manasa131326@gmail.com>
2023-09-30 17:32:04 +0530

    3rd commit

--- src/test/java/manasa/Sample1Test.java
+++ src/test/java/manasa/Sample1Test.java
@@ -1,1 +1,12 @@
 package manasa;
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+@Test(groups="Integration")
+public void sample() {
+
+	System.out.println("This is sample2");
+}
+}
+
--- src/test/java/manasa/Sample2Test.java
+++ src/test/java/manasa/Sample2Test.java
@@ -1,16 +1,11 @@
 package manasa;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Sample2Test {
 @Test(groups="smoke,Integration")
 public void sample() {
-	WebDriver driver =  new ChromeDriver();
-	driver.get("https://www.amazon.in/");
-			
-	
+
 	System.out.println("This is sample2");
 }
 }
