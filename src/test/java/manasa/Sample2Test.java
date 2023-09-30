2082a9ff0f95be9e69a798ecb9b5e288bab1cb34
ManasaHemanth <manasa131326@gmail.com>
2023-09-30 17:13:26 +0530

    bgv

---
+++ src/test/java/manasa/Sample2Test.java
@@ -0,0 +1,10 @@
+package manasa;
+
+import org.testng.annotations.Test;
+
+public class Sample2Test {
+@Test
+public void sample() {
+	System.out.println("This is sample2");
+}
+}
---
+++ src/test/java/manasa/Sample3Test.java
@@ -0,0 +1,10 @@
+package manasa;
+
+import org.testng.annotations.Test;
+
+public class Sample3Test {
+@Test
+public void Sample() {
+	System.out.println("This is sample 3");
+}
+}
